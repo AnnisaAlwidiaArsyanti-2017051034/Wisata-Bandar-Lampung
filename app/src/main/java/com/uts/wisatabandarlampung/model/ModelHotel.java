b44880fcd5ecef2c257c93f30e382ba6b4120233
AnnisaAlwidiaArsyanti-2017051034 <83524732+AnnisaAlwidiaArsyanti-2017051034@users.noreply.github.com>
2022-06-26 20:45:17 +0700

    Add files via upload

--- app/src/main/java/com/uts/wisatabandarlampung/model/ModelHotel.java
+++ app/src/main/java/com/uts/wisatabandarlampung/model/ModelHotel.java
@@ -1,1 +1,56 @@
+package com.uts.wisatabandarlampung.model;
 
+import java.io.Serializable;
+
+public class ModelHotel implements Serializable {
+
+    private String idHotel, txtNamaHotel, txtAlamatHotel, txtNoTelp, Koordinat, GambarHotel;
+    public String getIdHotel() {
+        return idHotel;
+    }
+
+    public void setIdHotel(String idHotel) {
+        this.idHotel = idHotel;
+    }
+
+
+    public String getTxtNamaHotel() {
+        return txtNamaHotel;
+    }
+
+    public void setTxtNamaHotel(String txtNamaHotel) {
+        this.txtNamaHotel = txtNamaHotel;
+    }
+
+    public String getTxtAlamatHotel() {
+        return txtAlamatHotel;
+    }
+
+    public void setTxtAlamatHotel(String txtAlamatHotel) {
+        this.txtAlamatHotel = txtAlamatHotel;
+    }
+
+    public String getTxtNoTelp() {
+        return txtNoTelp;
+    }
+
+    public void setTxtNoTelp(String txtNoTelp) {
+        this.txtNoTelp = txtNoTelp;
+    }
+
+    public String getKoordinat() {
+        return Koordinat;
+    }
+
+    public void setKoordinat(String koordinat) {
+        this.Koordinat = koordinat;
+    }
+
+    public String getGambarHotel() {
+        return GambarHotel;
+    }
+
+    public void setGambarHotel(String gambarHotel) {
+        this.GambarHotel = gambarHotel;
+    }
+}
