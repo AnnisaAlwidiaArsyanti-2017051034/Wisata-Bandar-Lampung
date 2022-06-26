5f2dd3be8b175cc133c32101ddcfac68187d0924
AnnisaAlwidiaArsyanti-2017051034 <83524732+AnnisaAlwidiaArsyanti-2017051034@users.noreply.github.com>
2022-06-26 20:43:08 +0700

    Add files via upload

--- app/src/main/java/com/uts/wisatabandarlampung/api/Api.java
+++ app/src/main/java/com/uts/wisatabandarlampung/api/Api.java
@@ -1,1 +1,13 @@
+package com.uts.wisatabandarlampung.api;
 
+public class Api {
+
+    public static String Hotel = "https://wisatabdl.000webhostapp.com/apihotel.php";
+    public static String DetailHotel ="https://wisatabdl.000webhostapp.com/apidetailhotel.php";
+    public static String Kuliner = "https://wisatabdl.000webhostapp.com/apikuliner.php";
+    public static String DetailKuliner = "https://wisatabdl.000webhostapp.com/apidetailkuliner.php";
+    public static String TempatIbadah = "https://wisatabdl.000webhostapp.com/apitempatibadah.php";
+    public static String Wisata = "https://wisatabdl.000webhostapp.com/apiwisata.php";
+    public static String DetailWisata = "https://wisatabdl.000webhostapp.com/apidetailwisata.php";
+
+}
